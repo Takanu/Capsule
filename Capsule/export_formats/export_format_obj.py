252c91f6935bf711f18459d507dd768b882c8f42
Takanu Kyriako <Takanu@users.noreply.github.com>
2023-04-22 03:22:17 +0100

    Export API - Updated FBX APIs for 3.5
    OBJ - Fixed "Apply Modifiers" not being used on OBJ exports
    OBJ - Added Export Colors
    OBJ - Added PBR Extensions option
    OBJ - Exposed Evaluation Mode property

--- Capsule/export_formats/export_format_obj.py
+++ Capsule/export_formats/export_format_obj.py
@@ -43,6 +43,16 @@ class CAP_FormatData_OBJ(PropertyGroup):
 		default = False,
 	)
 
+	evaluation_mode : EnumProperty(
+		name = "Evaluation Mode",
+		items = (
+			('DAG_EVAL_VIEWPORT', "Viewport (Default)", "Export objects as they appear in the Viewport"),
+			('DAG_EVAL_RENDER', "Render", "Export objects as they appear in the Render"),
+			),
+		description = "Determines properties like object visibility, modifiers etc., where they differ for Render and Viewport",
+		default = 'DAG_EVAL_VIEWPORT'
+    )
+
 
 	# transform
 
@@ -97,10 +107,10 @@ class CAP_FormatData_OBJ(PropertyGroup):
 		default = True,
 	)
 
-	export_materials: BoolProperty(
-		name = "Export Materials",
-		description = "Export a corresponding MTL library with the OBJ file. There must be a Principled-BSDF node for image textures to be exported to the MTL file",
-		default = True,
+	export_colors: BoolProperty(
+		name = "Export Colors",
+		description = "Export per-vertex colors",
+		default = False,
 	)
 
 	export_curves_as_nurbs: BoolProperty(
@@ -112,6 +122,19 @@ class CAP_FormatData_OBJ(PropertyGroup):
 	export_triangulated_mesh: BoolProperty(
 		name = "Triangulate Faces",
 		description = "All ngons with four or more vertices will be triangulated. Meshes in the scene will not be affected. Behaves like Triangulate Modifier with ngon-method: “Beauty”, quad-method: “Shortest Diagonal”, min vertices: 4",
+		default = False,
+	)
+
+
+	export_materials: BoolProperty(
+		name = "Export Materials",
+		description = "Export a corresponding MTL library with the OBJ file. There must be a Principled-BSDF node for image textures to be exported to the MTL file",
+		default = True,
+	)
+
+	export_pbr_extensions: BoolProperty(
+		name = "Use PBR Extension",
+		description = "Export Materials with PBR Extensions, Export MTL library using PBR extensions (roughness, metallic, sheen, clearcoat, anisotropy, transmission)",
 		default = False,
 	)
 
@@ -146,7 +169,7 @@ class CAP_FormatData_OBJ(PropertyGroup):
 			check_existing = False,
 			export_animation = export_preset.export_animation,
 			export_selected_objects = True,
-			export_eval_mode = 'DAG_EVAL_VIEWPORT',
+			apply_modifiers = export_preset.apply_modifiers,
 
 			# File
 			export_object_groups = self.export_object_groups,
@@ -154,6 +177,7 @@ class CAP_FormatData_OBJ(PropertyGroup):
 			export_vertex_groups  = self.export_vertex_groups,
 			export_smooth_groups = self.export_smooth_groups,
 			smooth_group_bitflags = self.smooth_group_bitflags,
+			export_eval_mode = self.evaluation_mode,
 
 			# Scene
 			global_scale = self.global_scale,
@@ -163,9 +187,12 @@ class CAP_FormatData_OBJ(PropertyGroup):
 			# Mesh
 			export_uv = self.export_uv,
 			export_normals = self.export_normals,
-			export_materials = self.export_materials,
+			export_colors = self.export_colors,
 			export_curves_as_nurbs = self.export_curves_as_nurbs,
 			export_triangulated_mesh = self.export_triangulated_mesh,
+
+			export_materials = self.export_materials,
+			export_pbr_extensions = self.export_pbr_extensions,
 
 			# Animation
 			start_frame = self.start_frame,
@@ -212,6 +239,11 @@ class CAP_FormatData_OBJ(PropertyGroup):
 			smooth_bitflags_option.active = exportData.export_smooth_groups
 			smooth_bitflags_option.prop(exportData, "smooth_group_bitflags")
 			smooth_bitflags_option.separator()
+			smooth_bitflags_option.separator()
+			
+			eval_mode_option = export_options.column(align = True)
+			eval_mode_option.prop(exportData, "evaluation_mode")
+			eval_mode_option.separator()
 
 
 		if cap_file.obj_menu_options == 'Scene':
@@ -238,11 +270,18 @@ class CAP_FormatData_OBJ(PropertyGroup):
 			mesh_options = export_options.column(align = True, heading = "Export")
 			mesh_options.prop(exportData, "export_uv")
 			mesh_options.prop(exportData, "export_normals")
-			mesh_options.prop(exportData, "export_materials")
+			mesh_options.prop(exportData, "export_colors")
 			mesh_options.prop(exportData, "export_curves_as_nurbs")
 			mesh_options.separator()
 			mesh_options.prop(exportData, "export_triangulated_mesh")
 			mesh_options.separator()
+
+			mat_options = export_options.column(align = True, heading = "Materials")
+			mat_options.prop(exportData, "export_materials")
+
+			mat_extension_options = export_options.column(align = True)
+			mat_extension_options.active = self.export_materials
+			mat_extension_options.prop(exportData, "export_pbr_extensions")
 		
 
 		elif cap_file.obj_menu_options == 'Animation':
